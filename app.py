42d860b4d2a7e06622da25abe1239d22e59141c1
Hoo-dkwozD <hoonderrick@gmail.com>
2025-11-05 16:31:15 +0800

    Addition of function section functionalities with syntax highlighting;

--- app.py
+++ app.py
@@ -6,13 +6,5 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/staging')
-def staging():
-    return render_template('staging.html')
-
-@app.route('/testing')
-def testing():
-    return render_template('testing.html')
-
 if __name__ == '__main__':
     app.run(debug=True)
